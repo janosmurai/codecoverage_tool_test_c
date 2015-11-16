19b254d90106c9e9e81c8e7657f4d9f1903f737d
janosmurai <murai.janos@gmail.com>
2015-11-16 21:41:18 +0100

    codecoverage test

---
+++ code_cov_test_c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 10;
+	int b = 20;
+	int i = 0;
+	int mult = 0;
+
+	if (a > b)
+	{
+		while (a > b)
+		{
+			b++;
+		}
+		printf("a is greater then b");
+	}
+	else if (a == b)
+	{
+		printf("a and b aer equal");
+	}
+	else
+	{
+		printf("b is greater then a");
+	}
+
+	for (i = 0; i < a; i++)
+	{
+		if (b && (a < 200))
+		{
+			mult = i * b;
+			printf("Mutliplication: %d", mult);
+		}
+	}
+}
